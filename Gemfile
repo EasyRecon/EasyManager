0876527faf74e148b7071abbb27b49cfc892bd23
JoMar <contact@jomar.fr>
2022-08-25 19:16:21 +0200

    Add SSH & SCP

--- Gemfile
+++ Gemfile
@@ -2,4 +2,9 @@
 
 source 'https://rubygems.org'
 
+gem 'bcrypt_pbkdf'
+gem 'ed25519'
+gem 'net-scp', '~> 4.0.0.rc1'
+gem 'net-ssh', '~> 7.0.0beta1'
 gem 'typhoeus'
+gem 'x25519'
---
+++ src/lib/ssh.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'net/ssh'
+require 'net/scp'
+
+class SrvManager
+  # SSH Class
+  class SSH
+    attr_reader :username, :ssh_key
+
+    def initialize(options = {})
+      @username = options[:username] || 'root'
+      @ssh_key = options[:ssh_key] || '~/.ssh/id_rsa'
+    end
+
+    def self.cmd_exec(ssh, srv, cmds)
+      cmd_values = {}
+
+      Net::SSH.start(srv['public_ip']['address'], ssh.username, keys: ssh.ssh_key) do |shell|
+        cmds.each do |cmd|
+          cmd_values[cmd] = shell.exec!(cmd).chomp
+        end
+      end
+
+      cmd_values
+    rescue Net::SSH::AuthenticationFailed
+      nil
+    end
+
+    def self.scp(ssh, srv, files)
+      Net::SCP.start(srv['public_ip']['address'], ssh.username, keys: ssh.ssh_key) do |shell|
+        files.each do |name, infos|
+          shell.upload! name, infos[:remote], recursive: infos[:recursive] || false
+        end
+      end
+    rescue Net::SSH::AuthenticationFailed
+      nil
+    end
+  end
+end
