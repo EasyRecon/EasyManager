8b24571a57f6f0e7c44dbbe758682882dd9479d8
JoMar <contact@jomar.fr>
2022-08-25 19:15:33 +0200

    Add file read & elapsed times utilities

--- src/lib/utilities.rb
+++ src/lib/utilities.rb
@@ -13,4 +13,14 @@ class Utilities
   def self.random_string
     (0...8).map { rand(97..122).chr }.join
   end
+
+  def self.file_read(file)
+    File.read(file)
+  rescue (Errno::ENOENT)
+    nil
+  end
+
+  def self.elapsed_times(first, second)
+    ((DateTime.parse(first) - DateTime.parse(second)) * 24 * 60 * 60).to_i
+  end
 end
